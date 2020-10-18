92c3c1fccebce864026ff042ecde068deef4198a
janynnegomes <janynnegomes@gmail.com>
2020-10-18 18:22:33 -0300

    5 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION } from "@angular/core";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  name = "Angular " + VERSION.major;
+
+  images = [
+    "https://http2.mlstatic.com/D_NQ_NP_2X_612946-MLA43654089811_102020-F.webp",
+    "https://http2.mlstatic.com/D_NQ_NP_2X_724968-MLA43654253363_102020-F.webp",
+    "https://http2.mlstatic.com/D_NQ_NP_2X_693004-MLA43654089813_102020-F.webp",
+    "https://http2.mlstatic.com/D_NQ_NP_2X_983687-MLA43654089814_102020-F.webp",
+    "https://http2.mlstatic.com/D_NQ_NP_2X_883221-MLA43654253371_102020-F.webp"
+  ];
 }
