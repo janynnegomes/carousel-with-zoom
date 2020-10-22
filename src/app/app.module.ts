5f4c08091b178f969e78d11acf44c20b62255bfc
janynnegomes <janynnegomes@gmail.com>
2020-10-21 23:29:14 -0300

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,11 @@ import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
-import { HelloComponent } from "./hello.component";
 import { ZoomCarouselComponent } from "./zoom-carousel/zoom-carousel.component";
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent, ZoomCarouselComponent],
+  declarations: [AppComponent, ZoomCarouselComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
